80d6a925522f380ba3e7ea42f663586873c2207d
YusifBashirzada <yusifsb-apa103@code.edu.az>
2026-03-15 18:14:40 +0400

    add custom array resize method

---
+++ 02-HeapStackRefOutArrayResize/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02_HeapStackRefOutArrayResize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5,};
+            int[] nums = { 6, 7, 8, 9, 10, 11, 12};
+            CustomArrResize(ref numbers, nums);
+        }
+    
+
+    public static void CustomArrResize(ref int[] arr, params int[] numsarr)
+    {
+            int[] newArr = new int[arr.Length + numsarr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+
+            for (int j = 0; j < numsarr.Length; j++)
+            {
+                 newArr[arr.Length + j] = numsarr[j];
+            }
+
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                Console.WriteLine(newArr[i]);
+            }
+        }
+    } 
+}
