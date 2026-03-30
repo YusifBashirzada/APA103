5580688ae500deaf2bc592947a9682fd2ea384f4
YusifBashirzada <yusifsb-apa103@code.edu.az>
2026-03-31 00:24:08 +0400

    create cafe system with nullable, enum, struct

---
+++ 07-NullableEnumStruct/Models/DrinkOrder.cs
@@ -0,0 +1,113 @@
+﻿using _07_NullableEnumStruct.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_NullableEnumStruct.Models
+{
+    public class DrinkOrder
+    {
+        public int OrderNumber;
+        public string CustomerName;
+        public DrinkType Type { get; set; }
+        public DrinkSize Size { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Price;
+
+        public DrinkOrder(int orderNumber, string customerName, DrinkType type, DrinkSize size) {
+            this.OrderNumber = orderNumber;
+            this.CustomerName = customerName;
+            this.Type = type;
+            this.Size = size;
+        }
+
+        public DrinkOrder()
+        {
+        }
+
+        public decimal CalculatePrice(DrinkType type, DrinkSize size)
+        {
+            decimal price = 0;
+            
+            switch(type)
+            {
+                case DrinkType.Coffee:
+                    switch(size)
+                    {
+                        case DrinkSize.Small:
+                            price = 3;
+                            break;
+                        case DrinkSize.Medium:
+                            price = 4; 
+                            break;
+                        case DrinkSize.Large:
+                            price = 5;
+                            break;
+                    }
+                    break;
+                case DrinkType.Tea:
+                    switch(size)
+                    {
+                        case DrinkSize.Small:
+                            price = 2;
+                            break;
+                        case DrinkSize.Medium:
+                            price = 3;
+                            break;
+                        case DrinkSize.Large:
+                            price = 4;
+                            break;
+                    }
+                    break;
+                case DrinkType.Juice:
+                    switch(size)
+                    {
+                        case DrinkSize.Small:
+                            price = 4;
+                            break;
+                        case DrinkSize.Medium:
+                            price = 5;
+                            break;
+                        case DrinkSize.Large:
+                            price = 6;
+                            break;
+                    }
+                    break;
+                case DrinkType.Water:
+                    switch (size)
+                    {
+                        case DrinkSize.Small:
+                            price = 1;
+                            break;
+                        case DrinkSize.Medium:
+                            price = 1.5m;
+                            break;
+                        case DrinkSize.Large:
+                            price = 2;
+                            break;
+                    }
+                    break;
+            }
+            this.Price = price;
+            return price;
+        }
+
+        public void UpdateStatus(OrderStatus newStatus)
+        {
+            this.Status = newStatus;
+        }
+
+        public void DisplayOrder()
+        {
+            Console.WriteLine($"Order Number: {OrderNumber}");
+            Console.WriteLine($"Customer Name: {CustomerName}");
+            Console.WriteLine($"Drink: {Type}");
+            Console.WriteLine($"Size: {Size}");
+            Console.WriteLine($"Price: {CalculatePrice(Type, Size)} AZN");
+            Console.WriteLine($"Status: {Status}");
+            Console.WriteLine("_______________________________________");
+        }
+    }
+}
---
+++ 07-NullableEnumStruct/Program.cs
@@ -0,0 +1,79 @@
+﻿using _07_NullableEnumStruct.Enums;
+using _07_NullableEnumStruct.Models;
+using System;
+using System.Drawing;
+
+namespace _07_NullableEnumStruct
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DrinkOrder order1 = new DrinkOrder(
+                101,
+                "Ali",
+                DrinkType.Coffee,
+                DrinkSize.Medium
+            );
+
+            DrinkOrder order2 = new DrinkOrder(
+                102,
+                "Leyla",
+                DrinkType.Tea,
+                DrinkSize.Large
+            );
+
+            DrinkOrder order3 = new DrinkOrder(
+                103, 
+                "Vüqar",
+                DrinkType.Juice,
+                DrinkSize.Small
+            );
+
+            order1.UpdateStatus(OrderStatus.Preparing);
+            order1.DisplayOrder();
+            order1.UpdateStatus(OrderStatus.Ready);
+            order1.DisplayOrder();
+            order1.UpdateStatus(OrderStatus.Delivered);
+            order1.DisplayOrder();
+
+            order2.UpdateStatus(OrderStatus.Ready);
+            order2.DisplayOrder();
+
+            order3.DisplayOrder();
+
+            Console.WriteLine("Bütün DrinkType deyerleri");
+            foreach (DrinkType type in Enum.GetValues(typeof(DrinkType)))
+            {
+                Console.WriteLine(type.ToString());
+                DrinkType drinkTypeParsed = (DrinkType)Enum.Parse(typeof(DrinkType), type.ToString());
+                Console.WriteLine(drinkTypeParsed);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Bütün DrinkSize deyerleri");
+            foreach (DrinkSize size in Enum.GetValues(typeof(DrinkSize)))
+            {
+                Console.WriteLine(size.ToString());
+                DrinkSize drinkSizeParsed = (DrinkSize)Enum.Parse(typeof(DrinkSize), size.ToString());
+                Console.WriteLine(drinkSizeParsed);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Bütün OrderStatus deyerleri");
+            foreach (OrderStatus order in Enum.GetValues(typeof(OrderStatus)))
+            {
+                Console.WriteLine(order);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Statistika");
+            Console.WriteLine("Umumi Sifaris: 3");
+            Console.WriteLine($"Birinci Sifarisin Qiymeti: {order1.CalculatePrice(order1.Type, order1.Size)} AZN");
+            Console.WriteLine($"Ikinci Sifarisin Qiymeti: {order2.CalculatePrice(order2.Type, order2.Size)} AZN");
+            Console.WriteLine($"Ucuncu Sifarisin Qiymeti: {order3.CalculatePrice(order3.Type, order3.Size)} AZN");
+            decimal totalprice = order1.Price + order2.Price + order3.Price;
+            Console.WriteLine($"Ümumi Mebleg: {totalprice} AZN");
+        }
+    }
+}
