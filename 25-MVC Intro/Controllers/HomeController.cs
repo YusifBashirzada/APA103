261b8422fe21abcf49c71c998cf62462086abedd
YusifBashirzada <yusifsb-apa103@code.edu.az>
2026-05-05 00:47:41 +0400

    do practise with mvc, learn create mvc project

---
+++ 25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index() {
+            //return "APA103";
+
+            //return Content("APA103");
+
+            //var student = new JsonResult(new { Id = 1, name = "Ali", surname = "Quliyev" });
+            //return student;
+
+            return View("Index");
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            if (id is null || id < 1)
+            {
+                return RedirectToAction(nameof(Error));
+            }
+            return RedirectToAction(nameof(Index), "Product");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
